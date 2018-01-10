8b7af4d37fdb3d56159871db3a48fc976a545d38
Juliana Rosa <32584367+rosaju@users.noreply.github.com>
2018-01-10 20:50:21 -0200

    Add files via upload

--- Revisão de lógica/revisao-logica.js
+++ Revisão de lógica/revisao-logica.js
@@ -1,51 +1,40 @@
+var clientes = []; //variável global, vista por todas as funções
 
- var clientes = [];
- 
- function cadastrarCliente() {
-     event.preventDefault();
+function cadastrarCliente() {
+    event.preventDefault();
+    
+    var objNome  = document.getElementById('nome');
+    //<input type="text" id="nome" value="valor que foi digitado" name="nome" placeholder="Seu nome aqui"><br>
+    var objIdade = document.getElementById('idade');
+    
+    var cli       = new Object();
+        cli.nome  = objNome.value;
+        cli.idade = objIdade.value;
+    
+    clientes.push(cli);
 
-     // var nome = document.getElementById('nome').value;
-     // var idade = document.getElementById('idade').value;
-     var objNome = document.getElementById('nome');
-     var objIdade = document.getElementById('idade');
-     
-     //var cli = {};
-     var cli = new Object();
-        cli.nome = objNome.nome;
-        cli.idade = objIdade.idade;
+    objNome.value   = '';
+    objIdade.value  = '';
+        
+    listarClientes();
+}
 
-        clientes.push(cli);
+function listarClientes() {
+    //gerar o template com o html dos clientes a ser
+        //listado
+        /*
+         <p>
+            nome : Eduardo<br>
+            idade: 15
+        </p>
+        */
+    event.preventDefault();
 
-        objNome.value = '';
-        objIdade.value = '';
+    var listaClientes = "";
 
-     // document.getElementById('nome').value = '';
-     // document.getElementById('idade').value = '';
+    for (var i = 0; i < clientes.length; i++) {
+        listaClientes += "<p>nome : "+clientes[i].nome+"<br>idade: "+clientes[i].idade+"</p>";
+    }
 
-     console.log(clientes);
-
-    /* var resultado;
-
-     if( idade >= 18){
-         resultado = 'Maior de idade';
-     } else {
-         resultado = 'Menor de idade';
-     }
-
-     var txt = "";
-     txt    += "<p>";
-     txt    += resultado;
-     txt    += "</p>";
-     document.getElementById('resultado').innerHTML = txt;
-     */
-
-     /*
-     document.getElementById('resultado').innerHTML = "<p>";
-     document.getElementById('resultado').innerHTML += resultado;
-     document.getElementById('resultado').innerHTML += "</p>"
-     console.log(resultado);
-     */
-
-
-     
- }
+    document.getElementById('resultado').innerHTML = listaClientes;
+}
