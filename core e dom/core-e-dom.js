426554fd004dc14468c3633432a1ad2e9e045388
JULIANA SOARES ROSA DE OLIVEIRA <05980940910@jpmorgan.sp>
2018-01-11 21:44:50 -0200

    corrigir

--- core e dom/core-e-dom.js
+++ core e dom/core-e-dom.js
@@ -1,1 +1,24 @@
-.
+
+var produtos = [
+    {//0
+    imagem : {
+        src   : ''
+        alt   : ''
+        title :  ''
+    },
+    descrição : 'Puma Golf Raglan Tech Polo Tee'
+    cor       : 'white'
+    preço     : 72
+    qtd       : 3
+    },
+]
+
+function listarProdutos(){
+    var template = "";
+
+    for(var i = 0; i < produtos.legth; i++){
+        template += 'div class="product">';
+
+    }
+    
+}
