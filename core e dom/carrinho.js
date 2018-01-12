cfa9c8d36b03cea1e885cfb35047289942a265b4
JULIANA SOARES ROSA DE OLIVEIRA <05980940910@jpmorgan.sp>
2018-01-12 19:31:18 -0200

    atualização

--- core e dom/carrinho.js
+++ core e dom/carrinho.js
@@ -1,24 +1,73 @@
-
+//dados reais viriam do back end (banco de dados)
 var produtos = [
     {//0
-    imagem : {
-        src   : ''
-        alt   : ''
-        title :  ''
+        codigo : 50,
+        imagem : {
+            src     : './img/camiseta.jpg',
+            alt     : "texto alternativo",
+            title   : "título da imagem"
+        },
+        descricao    : 'Puma Golf Raglan Tech Polo Tee',
+        cor         : 'Purple',
+        preco       : 72,
+        qtd         : 3
     },
-    descrição : 'Puma Golf Raglan Tech Polo Tee'
-    cor       : 'white'
-    preço     : 72
-    qtd       : 3
+    {//1
+        codigo : 99,
+        imagem : {
+            src     : './img/tenis.jpg',
+            alt     : "texto alternativo",
+            title   : "título da imagem"
+        },
+        descricao    : 'Nike Mens Running Shoe',
+        cor         : 'White',
+        preco       : 50,
+        qtd         : 1
+    },
+    {//2
+        codigo : 287,
+        imagem : {
+            src     : './img/jaqueta.jpg',
+            alt     : "texto alternativo",
+            title   : "título da imagem"
+        },
+        descricao    : 'DC Mens Axis Snowboard Jacket',
+        cor         : 'White',
+        preco       : 89.97,
+        qtd         : 2
     },
 ]
 
+
+/**
+    <div class="product">
+        <i class="fa fa-times-circle"></i>
+        <img src="img/camiseta.png" alt="t-shirt" class="product__img">
+        <span id="p1" class="product__name"></span>
+        <span id="c1" class="product__color"></span>
+        <span id="s1" class="product__size"></span>
+        <span id="v1" class="product__value"></span>*<input type="number" name="" id=""> = <span>$216</span>
+    </div>
+ */
 function listarProdutos(){
+ //Gerar o template através do array de objetos
     var template = "";
-
-    for(var i = 0; i < produtos.legth; i++){
-        template += 'div class="product">';
-
-    }
     
+    for(var i = 0; i < produtos.length; i++){
+        console.log( produtos[i] );
+        template += '<div class="product">';
+        template +=     '<i class="fa fa-times-circle"></i>';
+        template +=     '<img src="' +produtos[i].imagem.src+ '" alt="t-shirt" class="product__img">';
+        template +=     '<span id="p1" class="product__name">'+produtos[i].descricao+'</span>';
+        template +=     '<span id="c1" class="product__color">'+produtos[i].cor+'</span>';
+        template +=     '<span id="s1" class="product__size"></span>';
+        template +=     '<span id="v1" class="product__value"><strong>R$ </strong>'+produtos[i].preco+'</span>*<input type="number"  value="'+produtos[i].qtd+'" name="" id=""> = <span> <strong>R$ </strong>'+(produtos[i].qtd * produtos[i].preco)+'</span>';
+        template += '</div>';
+    }
+
+    document.getElementById("products").innerHTML = template;
+}
+
+function validaSexo(_id){
+    alert(_id);
 }
